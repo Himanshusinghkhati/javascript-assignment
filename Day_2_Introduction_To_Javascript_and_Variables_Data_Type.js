aa4a8f1f7e0d336e09c2a33b578ca6c5b29a0ea8
Himanshu <iamhimanshukhati@gmail.com>
2024-06-16 21:55:40 +0530

    Day-2-Assignment-Introduction to javascript and variables datatype

---
+++ Day_2_Introduction_To_Javascript_and_Variables_Data_Type.js
@@ -0,0 +1,54 @@
+//Problem 1:
+//Print "Ducat An IT Institute" in the console followed by "A Transformation in Education" in the next line.
+console.log("Ducat An IT Institute");
+console.log("A Transformation in Education");
+
+//Problem 2:
+//Define a variable called  name  with your Name as the assigned value
+//Print the value stored in the variable name
+//Change the variable to store your father's name
+//Print the value stored in the variable name
+//Change the variable again to store your mother's name.
+//Print the value stored in the variable name
+var name = "Himanshu";
+console.log(name);
+name = "Devendra";
+console.log(name);
+name = "Aditi";
+console.log(name);
+
+//Problem 3:
+//Create a variable  name  to store your name
+//Create a variable  age  to store your age
+//Print the values stored in the variable on one line followed by the type of the variable in the next line
+var name = "Himanshu";
+var age = 25;
+console.log(name, age);
+console.log(typeof name, typeof age);
+
+//Problem 4:
+//Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+//Print the report card of the student (You can make it look nice by using some keyboard symbols)
+var name = "Himanshu Khati";
+var school = "Army Public School";
+var grade = 10;
+var section = "A";
+var rollNo = 7;
+var marksMath = 55;
+var marksScience = 90;
+var marksEnglish = 68;
+console.log("-----------------------------------------------------");
+console.log("|                     REPORT CARD                   |");
+console.log("-----------------------------------------------------");
+console.log("Name    : " + name);
+console.log("School  : " + school);
+console.log("Grade   : " + grade);
+console.log("Section : " + section);
+console.log("Roll No : " + rollNo);
+console.log("-----------------------------------------------------");
+console.log("|                     SUBJECT MARKS                 |");
+console.log("-----------------------------------------------------");
+console.log("Math       : " + marksMath); 
+console.log("Science    : " + marksScience);
+console.log("English    : " + marksEnglish);
+
