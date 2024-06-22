fc167dab4ca9eeb0e380c3ca73251bdcf6e8ef37
Himanshu <119309525+Himanshusinghkhati@users.noreply.github.com>
2024-06-22 21:56:24 +0530

    Day_4_Comparison_and_Relational_Operators_Assignment.js

---
+++ Day_4_Comparison_and_Relational_Operators_Assignment.js
@@ -0,0 +1,38 @@
+//Problem 1: Greet the User
+//Description: Create a JavaScript program to greet the user by their name.
+//Suggested Name: "Name Greeting"
+var name = "Himanshu";
+var greeting = " Welcome to my page";
+var result = name + greeting;
+console.log("Name Greeting = " +result);
+
+//Problem 2: Calculate Rectangle Area
+//Description: Develop a program to calculate the area of a rectangle using user-provided dimensions.
+//Suggested Name: "Rectangle Area Calculator"
+var firstNo = 2;
+var secondNo = 3;
+var area = firstNo * secondNo;
+console.log("Rectangle Area Calculator = " + area);
+
+//Problem 3: Compare Two Numbers
+//Description: Write a program to compare two numbers and determine if they are equal.
+//Suggested Name: "Number Comparison"
+var num1 = 11;
+var num2 = 10;
+var compare = num1 === num2;
+console.log("Number Comparison = " +compare);
+
+//Problem 4: Voting Eligibility Check
+//Description: Create a program to check if a user is eligible to vote based on their age.
+//Suggested Name: "Voting Eligibility Checker"
+var value = 21;
+var votingAge = value >= 18;
+console.log("Voting Eligibility Checker = " + votingAge );
+
+//Problem 5: Shopping Budget Evaluation
+//Description: Design a program to evaluate if a user can afford items within a specified budget.
+//Suggested Name: "Budget Shopping Evaluation"
+var itemPrice = 150;
+var userBudget = 75;
+var canAfford = userBudget >= itemPrice;
+console.log("Budget Shopping Evaluation = " + canAfford);
